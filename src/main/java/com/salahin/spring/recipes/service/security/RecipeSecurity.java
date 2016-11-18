f3d094a77ea2341c23b6779f4d9c1353150c2728
rocky-bgta <rocky.bgta@gmail.com>
2016-11-18 17:58:05 +0600

    jdbc security implementation

--- src/main/java/com/salahin/spring/recipes/service/security/RecipeSecurity.java
+++ src/main/java/com/salahin/spring/recipes/service/security/RecipeSecurity.java
@@ -1,10 +1,14 @@
 package com.salahin.spring.recipes.service.security;
 
+import com.salahin.spring.recipes.service.presistanceDataSource.PersistenceDataSourceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 /**
  * Created by Tuli on 11/15/2016.
@@ -13,6 +17,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class RecipeSecurity extends WebSecurityConfigurerAdapter  {
 
+    @Autowired
+    private PersistenceDataSourceProvider persistenceDataSourceProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -32,7 +38,10 @@ public class RecipeSecurity extends WebSecurityConfigurerAdapter  {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("a").authorities("ROLE_USER");
+      /* auth.inMemoryAuthentication()
+                .withUser("admin").password("a").authorities("ROLE_USER");*/
+
+        auth.jdbcAuthentication()
+                .dataSource(persistenceDataSourceProvider.mySqlDataSource());
     }
 }
