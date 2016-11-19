f2ae9cc239bf6327418e216cecc613b17a240d12
rocky-bgta <rocky.bgta@gmail.com>
2016-11-19 12:32:15 +0600

    EhCache implementation

--- src/main/java/com/salahin/spring/recipes/service/config/ServiceConfiguration.java
+++ src/main/java/com/salahin/spring/recipes/service/config/ServiceConfiguration.java
@@ -1,13 +1,7 @@
 package com.salahin.spring.recipes.service.config;
 
 import com.salahin.spring.recipes.service.cacheProvider.EhCacheProvider;
-import com.salahin.spring.recipes.service.presistanceDataSource.PersistenceDataSourceProvider;
-import com.salahin.spring.recipes.service.recipeimplementation.HomeMadeServiceImple;
-import com.salahin.spring.recipes.service.recipeimplementation.MemberServiceImple;
-import com.salahin.spring.recipes.service.recipeinterface.HomeMadeServiceInterface;
-import com.salahin.spring.recipes.service.recipeinterface.MemberServiceInterface;
 import com.salahin.spring.recipes.service.springSocialIntegration.config.SocialConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
