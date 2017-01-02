13a772f8caefa7f7a471432dc5f0d6ccf2611aa7
rocky-bgta <rocky.bgta@gmail.com>
2017-01-02 10:33:04 +0600

    Test functionality implementation

--- src/test/java/com/salahin/spring/recipes/test/spring/annotations/PrototypeBeanTest.java
+++ src/test/java/com/salahin/spring/recipes/test/spring/annotations/PrototypeBeanTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
